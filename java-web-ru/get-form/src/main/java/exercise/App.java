0cb4d89b866346b7b966deeb20bec6c6fdd44e2b
@hexlet/cli <support@hexlet.io>
2024-02-12 07:32:38 +0100

    submit java-web-ru/get-form

---
+++ java-web-ru/get-form/src/main/java/exercise/App.java
@@ -0,0 +1,47 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import exercise.model.User;
+import exercise.dto.users.UsersPage;
+import java.util.Collections;
+import org.apache.commons.lang3.StringUtils;
+
+public final class App {
+
+    // Каждый пользователь представлен объектом класса User
+    private static final List<User> USERS = Data.getUsers();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        // BEGIN
+        app.get("/users", ctx -> {
+            var term = ctx.queryParam("term");
+            var page = new UsersPage(USERS, term);
+            if (term != null) {
+                var search = term.toLowerCase();
+                var filteredUsers = USERS.stream()
+                        .filter(user -> user.getFirstName().toLowerCase().contains(search))
+                        .toList();
+                page = new UsersPage(filteredUsers, term);
+            }
+            ctx.render("users/index.jte", Collections.singletonMap("page", page));
+        });
+        // END
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
